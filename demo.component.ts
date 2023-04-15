d008c6e9779ddbc5cab8d087cc0213033e5631e6
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-04-16 00:02:34 +0530

    Add files via upload

---
+++ demo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent {
+  public Marvellous = "Demo"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fun() {
+    return  + this.Marvellous;
+  }
+}
+
